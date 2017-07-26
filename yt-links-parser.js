0f5c642681abfbf83841cbfcadfbc108de19cc1c
AD\bukowski-piotr <pbukowski@ganymede.eu>
2017-07-26 13:15:33 +0200

    change thumbnail code: img tag replaced with background-image style; refactor js code; change jquery source to cdn

--- yt-links-parser.js
+++ yt-links-parser.js
@@ -1,158 +1,161 @@
-(function ( $ ) {
+(function ($) {
 
-	$.fn.ytLinksParseToImage = function(options) {
-		var settings = $.extend({
-			width: 480,
-			height: 380
-		}, options );
-		
-		settings.replace_html = '<div class="yt-links-parser-container" data-yt-code="##CODE##"><img src="//img.youtube.com/vi/##CODE##/hqdefault.jpg" style="width:##WIDTH##px;height:##HEIGHT##px"><div class="yt-links-parser-play"></div></div>';
-		
-		return this.ytLinksParse(settings).find('.yt-links-parser-container').on('click', function() {
-			var el = jQuery(this);
-			var w = el.width();
-			var h = el.height();
-			var code = el.attr('data-yt-code');
-			el.replaceWith('<iframe width="'+w+'" height="'+h+'" src="//www.youtube.com/embed/'+code+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-			return false;
-		});
-	};
+    $.fn.ytLinksParseToImage = function(options) {
+        var settings = $.extend({
+            width: 640,
+            height: 360
+        }, options);
 
-	$.fn.ytLinksParse = function(options) {
-		
-		var settings = $.extend({
-			width: 480,
-			height: 380,
-			replace_html: '<iframe width="##WIDTH##" height="##HEIGHT##" src="//www.youtube.com/embed/##CODE##" frameborder="0" allowfullscreen></iframe>'
-		}, options );
-		
-		var invalid_matches_beginnings = ['src=', 'value=', 'href='];
-		
-		var get_replace_html = function(code) {
-			return settings.replace_html.replace(/##WIDTH##/g, settings.width).replace(/##HEIGHT##/g, settings.height).replace(/##CODE##/g, code);
-		}
-		
-		var is_false_positive = function(match) {
-			for (var i=0; i<invalid_matches_beginnings.length; i++)
-			{
-				if (match.substring(0, invalid_matches_beginnings[i].length) == invalid_matches_beginnings[i])
-					return true;
-			}
-			return false;
-		}
-		
-		var replace_matches = function(matches, html) {
-			var new_html = '';
-			
-			var offset = 0;
-			
-			for(var i=0; i<matches.length; i++) {
-				var match = matches[i];
-				new_html += html.slice(offset, match.offset) + get_replace_html(match.video_code);
-				offset = match.offset + match.length;
-			}
-			new_html += html.slice(offset, html.length);
-			
-			return new_html;
-		}
-		
-		var get_yt_host = function(match) {
-			var host = match.split('/');
-			if (host.length < 2)
-				return '';
-				
-			return host[0];
-		}
-		
-		var get_video_code = function(match) {
-			match = remove_protocol(match);
-		
-			var host = get_yt_host(match);
-			match = remove_host(match, host);
-			
-			if (!host || !match)
-				return '';
-			
-			if (host.replace('www.', '') == 'youtu.be') {
-				return get_youtu_be_code(match);
-			}
-			
-			var match_arr = match.split('/');
-			
-			if (match_arr.length > 1) {
-				return get_embed_or_v_code(match_arr);
-			}
-			
-			match_arr = match.split('?');
-			if (match_arr[0] == 'watch' && match_arr.length > 1) {
-				return get_watch_code(match_arr);
-			}
-			
-			return '';
-		}
-		
-		var remove_protocol = function(match) {
-			return match.replace(/(https?:)?\/\//i, '');
-		}
-		
-		var remove_host = function(match, host) {
-			return match = match.replace(host+'/', '');
-		}
-		
-		var get_youtu_be_code = function(match) {
-			match = match.split('?');
-			return match[0];
-		}
-		
-		var get_embed_or_v_code = function(match_arr) {
-			if (match_arr[0] = 'v' || match_arr[0] == 'embed') {
-				match_arr = match_arr[1].split('?');
-				return match_arr[0];
-			}
-			return '';
-		}
-		
-		var get_watch_code = function(match_arr) {
-			match_arr = match_arr[1].split('&');
-			for (var i=0; i<match_arr.length; i++) {
-				var pair = match_arr[i].split('=');
-				if (pair.length !=2) {
-					continue;
-				}
-				if (pair[0] == 'v') {
-					return pair[1];
-				}
-			}
-			return '';
-		}
-		
-		var remove_ending_from_match = function(match) {
-			var last_char = match.charAt(match.length-1);
-			
-			if (last_char.search(/'|"|<|\s/g) != -1) {
-				match = match.substr(0, match.length-1);
-			}
-			return match;
-		}
-		
-		return this.each(function() {
-			var html = $(this).html();
-			var pattern = /(href=['"]?|src=['"]?|value=['"]?)?((https?:)?\/\/){0,1}(www.youtube.com|youtu.be)\/(.+?)('|"|<|\s|$)/gi;
-			
-			var matches = [];
-			var result = true;
-			while(result = pattern.exec(html)) {
-				var match = remove_ending_from_match(result[0]);
-				
-				if (is_false_positive(match))
-					continue;
-				var video_code = get_video_code(match);
-				if (video_code) {
-					matches.push({video_code: video_code, offset: result['index'], length: match.length});
-				}
-			}
-			html = replace_matches(matches, html);
-			$(this).html(html);
-		});
-	};
-}( jQuery ));
+        //settings.replaceHtml = '<div class="yt-links-parser-container" data-yt-code="##CODE##"><img src="https://i.ytimg.com/vi/##CODE##/hqdefault.jpg" style="width:##WIDTH##px;height:##HEIGHT##px"><div class="yt-links-parser-play"></div></div>';
+        settings.replaceHtml = '<div class="yt-links-parser-container" data-yt-code="##CODE##" style="background-image:url(\'https://i.ytimg.com/vi/##CODE##/hqdefault.jpg\');width:##WIDTH##px;height:##HEIGHT##px"><div class="yt-links-parser-play"></div></div>';
+
+        return this.ytLinksParse(settings).find('.yt-links-parser-container').off('click.play').on('click.play', function() {
+            var el = jQuery(this);
+            var w = el.width();
+            var h = el.height();
+            var code = el.attr('data-yt-code');
+            el.replaceWith('<iframe width="' + w + '" height="' + h + '" src="//www.youtube.com/embed/' + code + '?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+            return false;
+        });
+    };
+
+    $.fn.ytLinksParse = function(options) {
+
+        var settings = $.extend({
+            width: 480,
+            height: 380,
+            replaceHtml: '<iframe width="##WIDTH##" height="##HEIGHT##" src="//www.youtube.com/embed/##CODE##" frameborder="0" allowfullscreen></iframe>'
+        }, options);
+
+        var invalidMatchesBeginnings = ['src=', 'value=', 'href='];
+
+        function getReplaceHtml(code) {
+            return settings.replaceHtml.replace(/##WIDTH##/g, settings.width).replace(/##HEIGHT##/g, settings.height).replace(/##CODE##/g, code);
+        }
+
+        function isFalsePositive(match) {
+            for (var i = 0; i < invalidMatchesBeginnings.length; i++) {
+                if (match.substring(0, invalidMatchesBeginnings[i].length) == invalidMatchesBeginnings[i]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function replaceMatches(matches, html) {
+            var newHtml = '';
+            var offset = 0;
+
+            for (var i = 0; i < matches.length; i++) {
+                var match = matches[i];
+                newHtml += html.slice(offset, match.offset) + getReplaceHtml(match.videoCode);
+                offset = match.offset + match.length;
+            }
+            newHtml += html.slice(offset, html.length);
+
+            return newHtml;
+        }
+
+        function geYtHost(match) {
+            var host = match.split('/');
+            if (host.length < 2) {
+                return '';
+            }
+            return host[0];
+        }
+
+        function getVideoCode(match) {
+            match = removeProtocol(match);
+
+            var host = geYtHost(match);
+            match = removeHost(match, host);
+
+            if (!host || !match) {
+                return '';
+            }
+
+            if (host.replace('www.', '') == 'youtu.be') {
+                return getYoutuBeCode(match);
+            }
+
+            var matchArray = match.split('/');
+
+            if (matchArray.length > 1) {
+                return getEmbedOrVCode(matchArray);
+            }
+
+            matchArray = match.split('?');
+            if (matchArray[0] == 'watch' && matchArray.length > 1) {
+                return getWatchCode(matchArray);
+            }
+
+            return '';
+        }
+
+        function removeProtocol(match) {
+            return match.replace(/(https?:)?\/\//i, '');
+        }
+
+        function removeHost(match, host) {
+            return match = match.replace(host+'/', '');
+        }
+
+        function getYoutuBeCode(match) {
+            match = match.split('?');
+            return match[0];
+        }
+
+        function getEmbedOrVCode(matchArray) {
+            if (matchArray[0] = 'v' || matchArray[0] == 'embed') {
+                matchArray = matchArray[1].split('?');
+                return matchArray[0];
+            }
+            return '';
+        }
+
+        function getWatchCode(matchArray) {
+            matchArray = matchArray[1].split('&');
+            for (var i = 0; i < matchArray.length; i++) {
+                var pair = matchArray[i].split('=');
+                if (pair.length !=2) {
+                    continue;
+                }
+                if (pair[0] == 'v') {
+                    return pair[1];
+                }
+            }
+            return '';
+        }
+
+        function removeEndingFromMatch(match) {
+            var lastChar = match.charAt(match.length-1);
+
+            if (lastChar.search(/'|"|<|\s/g) != -1) {
+                match = match.substr(0, match.length-1);
+            }
+            return match;
+        }
+
+        return this.each(function() {
+            var html = $(this).html();
+            var pattern = /(href=['"]?|src=['"]?|value=['"]?)?((https?:)?\/\/){0,1}(www.youtube.com|youtu.be)\/(.+?)('|"|<|\s|$)/gi;
+
+            var matches = [];
+            var result = true;
+            while(result = pattern.exec(html)) {
+                var match = removeEndingFromMatch(result[0]);
+
+                if (isFalsePositive(match)) {
+                    continue;
+                }
+
+                var videoCode = getVideoCode(match);
+                if (videoCode) {
+                    matches.push({videoCode: videoCode, offset: result['index'], length: match.length});
+                }
+            }
+            html = replaceMatches(matches, html);
+            $(this).html(html);
+        });
+    };
+}(jQuery));
